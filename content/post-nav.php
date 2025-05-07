180e0515c857287a225d93e60cb34cd159dfc454
stationaryreality <stationaryreality@tutanota.com>
2025-05-07 16:03:14 +0000

    Fixed Post Navigation image spacing

--- content/post-nav.php
+++ content/post-nav.php
@@ -2,10 +2,14 @@
 
 <div id="loop-container" class="loop-container">
   <div class="tag-posts-grid">
+    <!-- your tag post items go here -->
+  </div>
 
+  <!-- Move this OUTSIDE of the grid -->
+  <div class="post-navigation-container">
     <!-- Previous Post -->
     <?php
-    $previous_post = get_adjacent_post( false, '', true ); // Get the previous post
+    $previous_post = get_adjacent_post( false, '', true );
     if ( $previous_post ) : ?>
       <div class="previous-post">
         <h2>Next Post</h2>
@@ -20,7 +24,7 @@
 
     <!-- Next Post -->
     <?php
-    $next_post = get_adjacent_post( false, '', false ); // Get the next post
+    $next_post = get_adjacent_post( false, '', false );
     if ( $next_post ) : ?>
       <div class="next-post">
         <h2>Previous Post</h2>
@@ -32,8 +36,8 @@
         <h3><a href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo get_the_title( $next_post->ID ); ?></a></h3>
       </div>
     <?php endif; ?>
-  
   </div>
 </div>
+
 
 <?php get_footer(); ?>
