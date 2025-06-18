c297116a2061b3e395ecfd9c598eb03f9e7998a8
stationaryreality <stationaryreality@tutanota.com>
2025-06-18 00:37:23 +0000

    added profile cpt to replace people/person/author confusion

---
+++ content-profile.php
@@ -0,0 +1,23 @@
+<?php
+$bio      = get_field('bio'); // Optional ACF field
+$portrait = get_field('portrait_image', get_the_ID());
+$img_url  = $portrait ? $portrait['sizes']['medium'] : '';
+?>
+
+<div class="person-content">
+  <?php if ($img_url): ?>
+    <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title(); ?>" class="author-thumbnail">
+  <?php endif; ?>
+
+  <h1><?php the_title(); ?></h1>
+
+  <div class="person-bio">
+    <?php if ($bio): ?>
+      <?php echo wp_kses_post($bio); ?>
+    <?php else: ?>
+      <?php the_content(); ?>
+    <?php endif; ?>
+  </div>
+
+  <?php get_template_part('content/profile-nav'); ?>
+</div>
---
+++ page-people-referenced.php
@@ -0,0 +1,39 @@
+<?php
+/* Template Name: People Referenced */
+get_header();
+?>
+
+<div class="author-grid">
+<?php
+$profiles = new WP_Query(array(
+  'post_type'      => 'profile',
+  'posts_per_page' => -1,
+  'orderby'        => 'title',
+  'order'          => 'ASC',
+));
+
+if ($profiles->have_posts()):
+  while ($profiles->have_posts()): $profiles->the_post();
+    $bio      = get_field('bio');           // Optional ACF field
+    $portrait = get_field('portrait_image'); // ACF image (returning array)
+    $img_url = $portrait ? $portrait['sizes']['thumbnail'] : '';
+    ?>
+    <div class="book-item">
+      <a href="<?php the_permalink(); ?>">
+        <?php if ($img_url): ?>
+          <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title(); ?>">
+        <?php endif; ?>
+        <h3><?php the_title(); ?></h3>
+      </a>
+      <?php if ($bio): ?>
+        <p><?php echo esc_html(wp_trim_words($bio, 20)); ?></p>
+      <?php endif; ?>
+    </div>
+    <?php
+  endwhile;
+  wp_reset_postdata();
+endif;
+?>
+</div>
+
+<?php get_footer(); ?>
