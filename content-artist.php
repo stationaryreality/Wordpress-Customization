6f10146f841fec635b1fb70dda3b05ae66c5fc31
stationaryreality <stationaryreality@tutanota.com>
2025-06-20 01:27:46 +0000

    added wikislugs

--- content-artist.php
+++ content-artist.php
@@ -1,7 +1,26 @@
 <?php
 $bio = get_field('bio'); // Optional ACF field
 $portrait = get_field('portrait_image', get_the_ID());
-$img_url  = $portrait ? $portrait['sizes']['medium'] : '';
+$img_url = $portrait ? $portrait['sizes']['thumbnail'] : '';
+$wiki_slug = get_field('wikipedia_slug');
+
+// Function to fetch Wikipedia summary
+function get_wikipedia_intro($slug) {
+    $api_url = "https://en.wikipedia.org/api/rest_v1/page/summary/" . urlencode($slug);
+
+    $response = wp_remote_get($api_url);
+
+    if (is_wp_error($response)) return false;
+
+    $body = wp_remote_retrieve_body($response);
+    $data = json_decode($body, true);
+
+    if (!empty($data['extract'])) {
+        return esc_html($data['extract']);
+    }
+
+    return false;
+}
 ?>
 
 <div class="person-content">
@@ -14,10 +33,12 @@ $img_url  = $portrait ? $portrait['sizes']['medium'] : '';
   <div class="person-bio">
     <?php if ($bio): ?>
       <?php echo wp_kses_post($bio); ?>
+    <?php elseif ($wiki_slug): ?>
+      <p><?php echo get_wikipedia_intro($wiki_slug); ?></p>
     <?php else: ?>
       <?php the_content(); ?>
     <?php endif; ?>
   </div>
-
+  
   <?php get_template_part('content/artist-nav'); ?>
 </div>
