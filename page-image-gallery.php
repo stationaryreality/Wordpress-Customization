742e20946c3a02c3f6b84a90cef369ca4aff2a5b
Stationary Reality <stationaryreality@tutanota.com>
2025-08-09 20:06:28 -0400

    fixed image gallery to be grid

--- page-image-gallery.php
+++ page-image-gallery.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
 
-<div class="image-grid">
+<div class="cited-grid cited-grid--images">
 <?php
 $images = new WP_Query(array(
   'post_type'      => 'image',
@@ -15,20 +15,28 @@ $images = new WP_Query(array(
 if ($images->have_posts()):
   while ($images->have_posts()): $images->the_post();
     $caption = get_field('image_caption');
-    $image   = get_field('image_file');
-    $img_url = $image ? $image['sizes']['thumbnail'] : '';
+    $image   = get_field('image_file'); // ACF image array
+    // choose a size consistent with books (change 'medium' if you prefer)
+$img_url = $image ? $image['url'] : '';
     ?>
-    <div class="book-item">
-      <a href="<?php the_permalink(); ?>">
+    <article id="post-<?php the_ID(); ?>" <?php post_class('cited-item'); ?>>
+      <a class="cited-item__link" href="<?php the_permalink(); ?>">
         <?php if ($img_url): ?>
-          <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title(); ?>" style="width:150px; height:150px; object-fit:cover;">
+          <div class="cited-item__thumb" aria-hidden="true">
+            <img src="<?php echo esc_url($img_url); ?>"
+                 alt="<?php echo esc_attr( get_the_title() ); ?>"
+                 loading="lazy"
+                 decoding="async" />
+          </div>
         <?php endif; ?>
-        <h3><?php the_title(); ?></h3>
+
+        <h3 class="cited-item__title"><?php the_title(); ?></h3>
       </a>
+
       <?php if ($caption): ?>
-        <p><?php echo esc_html(wp_trim_words($caption, 20)); ?></p>
+        <p class="cited-item__meta"><?php echo esc_html( wp_trim_words( $caption, 20 ) ); ?></p>
       <?php endif; ?>
-    </div>
+    </article>
     <?php
   endwhile;
   wp_reset_postdata();
