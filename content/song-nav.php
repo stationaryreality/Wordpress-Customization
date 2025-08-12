9e96e88737556f9a49b512791eaff96e07704947
Stationary Reality <stationaryreality@tutanota.com>
2025-08-12 15:27:07 -0400

    deleted feauture level, migrated to taxnomomy

--- content/song-nav.php
+++ content/song-nav.php
@@ -1,12 +1,72 @@
 <?php
 $current_id = get_the_ID();
-$song_ids = get_posts([
-  'post_type' => 'song',
-  'numberposts' => -1,
-  'orderby' => 'title',
-  'order' => 'ASC',
-  'fields' => 'ids',
-]);
+
+// Check if this is a rap song
+$is_rap = has_term( 'rap', 'song_category', $current_id );
+
+// If rap, get all rap songs (regardless of tier)
+if ( $is_rap ) {
+    $song_ids = get_posts([
+        'post_type' => 'song',
+        'numberposts' => -1,
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'fields' => 'ids',
+        'tax_query' => [
+            [
+                'taxonomy' => 'song_category',
+                'field'    => 'slug',
+                'terms'    => 'rap',
+            ]
+        ],
+    ]);
+
+    $heading_prefix = 'Rap Song';
+
+} else {
+    // Non-rap: find this song's feature level
+    $terms = wp_get_post_terms( $current_id, 'feature_level', ['fields' => 'slugs'] );
+    $tier = $terms[0] ?? '';
+
+    // Query only songs in the same tier and NOT rap
+    $song_ids = get_posts([
+        'post_type' => 'song',
+        'numberposts' => -1,
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'fields' => 'ids',
+        'tax_query' => [
+            'relation' => 'AND',
+            [
+                'taxonomy' => 'feature_level',
+                'field'    => 'slug',
+                'terms'    => $tier,
+            ],
+            [
+                'taxonomy' => 'song_category',
+                'field'    => 'slug',
+                'terms'    => 'rap',
+                'operator' => 'NOT IN',
+            ]
+        ],
+    ]);
+
+    // Set heading prefix based on tier
+    switch ( $tier ) {
+        case 'narrative':
+            $heading_prefix = 'Narrative Song';
+            break;
+        case 'featured':
+            $heading_prefix = 'Featured Song';
+            break;
+        case 'referenced':
+            $heading_prefix = 'Referenced Song';
+            break;
+        default:
+            $heading_prefix = 'Song';
+            break;
+    }
+}
 
 $current_index = array_search($current_id, $song_ids);
 $next_id = $song_ids[$current_index + 1] ?? null;
@@ -16,7 +76,7 @@ $prev_id = $song_ids[$current_index - 1] ?? null;
 <div class="post-navigation-container song-nav" style="display: flex; justify-content: center; gap: 60px; margin-top: 60px;">
   <?php if ($next_id): ?>
     <div class="previous-post" style="text-align: center;">
-      <h2>Next Song</h2>
+      <h2>Next <?php echo esc_html($heading_prefix); ?></h2>
       <a href="<?php echo get_permalink($next_id); ?>" style="display: inline-block;">
         <?php
         $cover = get_field('cover_image', $next_id);
@@ -31,7 +91,7 @@ $prev_id = $song_ids[$current_index - 1] ?? null;
 
   <?php if ($prev_id): ?>
     <div class="next-post" style="text-align: center;">
-      <h2>Previous Song</h2>
+      <h2>Previous <?php echo esc_html($heading_prefix); ?></h2>
       <a href="<?php echo get_permalink($prev_id); ?>" style="display: inline-block;">
         <?php
         $cover = get_field('cover_image', $prev_id);
