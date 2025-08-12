e33bea072070177953ba540ea79625aaf028e3a5
Stationary Reality <stationaryreality@tutanota.com>
2025-08-11 21:30:58 -0400

    added featured level taxonomy and rapper designation for artists cpts, will delete rapper cpt and use them to sort on pages

--- page-artists-featured.php
+++ page-artists-featured.php
@@ -5,76 +5,105 @@ get_header();
 
 <main id="primary" class="site-main artist-rapper-archive">
 
-  <!-- Artists Featured -->
   <section class="cpt-section">
-    <h2 class="cpt-group-label">Artists Featured</h2>
 
     <?php
     $artist_tiers = [
-      'primary'    => '⭐ Primary',
-      'featured'   => '🎧 Featured',
-      'referenced' => '🎤 Referenced',
+      'narrative'  => '📖 Narrative Thread Artists',
+      'featured'   => '🎧 Featured Artists',
+      'referenced' => '🎤 Referenced Artists',
     ];
 
-    foreach ($artist_tiers as $level => $label):
-      $artists = new WP_Query([
-        'post_type'      => 'artist',
-        'posts_per_page' => -1,
-        'meta_key'       => 'feature_level',
-        'meta_value'     => $level,
-        'orderby'        => 'title',
-        'order'          => 'ASC',
-      ]);
+    // First: Non-rappers grouped by feature_level
+    foreach ($artist_tiers as $feature_slug => $feature_label):
+      ?>
+      <div class="feature-group">
+        <h3 class="feature-level"><?php echo esc_html($feature_label); ?></h3>
 
-      if ($artists->have_posts()): ?>
-        <div class="feature-group">
-          <h3 class="feature-level"><?php echo $label; ?></h3>
+        <?php
+        $non_rappers = new WP_Query([
+          'post_type'      => 'artist',
+          'posts_per_page' => -1,
+          'tax_query'      => [
+            'relation' => 'AND',
+            [
+              'taxonomy' => 'feature_level',
+              'field'    => 'slug',
+              'terms'    => $feature_slug,
+            ],
+            [
+              'taxonomy' => 'artist_type',
+              'field'    => 'slug',
+              'terms'    => ['rapper'],
+              'operator' => 'NOT IN',
+            ],
+          ],
+          'orderby'        => 'title',
+          'order'          => 'ASC',
+        ]);
+
+        if ($non_rappers->have_posts()): ?>
           <div class="author-grid">
-            <?php while ($artists->have_posts()): $artists->the_post();
+            <?php while ($non_rappers->have_posts()): $non_rappers->the_post();
               $portrait = get_field('portrait_image');
               $img_url = $portrait ? $portrait['sizes']['thumbnail'] : '';
             ?>
               <div class="book-item">
                 <a href="<?php the_permalink(); ?>">
                   <?php if ($img_url): ?>
-                    <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title(); ?>">
+                    <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title_attribute(); ?>">
                   <?php endif; ?>
                   <h3><?php the_title(); ?></h3>
                 </a>
               </div>
             <?php endwhile; ?>
           </div>
-        </div>
-        <?php wp_reset_postdata();
-      endif;
-    endforeach;
-    ?>
+        <?php
+        endif;
+        wp_reset_postdata();
+        ?>
+
+      </div> <!-- .feature-group -->
+
+    <?php endforeach; ?>
+
   </section>
 
-  <!-- Rappers Featured -->
-  <section class="cpt-section">
-<h2 id="rappers" class="section-heading">Rappers Featured</h2>
+  <hr>
+
+  <section class="cpt-section rappers-section">
+    <h2>🎤 Rappers Featured</h2><BR>
 
     <?php
-    $rapper_tiers = [
-      'primary'    => '⭐ Primary',
-      'featured'   => '🎧 Featured',
-      'referenced' => '🎤 Referenced',
-    ];
+    // Then: Rappers grouped by feature_level
+    foreach ($artist_tiers as $feature_slug => $feature_label):
+      ?>
+      <div class="feature-group rapper-group">
+        <h3 class="feature-level"><?php echo esc_html($feature_label); ?></h3>
 
-    foreach ($rapper_tiers as $level => $label):
-      $rappers = new WP_Query([
-        'post_type'      => 'rapper',
-        'posts_per_page' => -1,
-        'meta_key'       => 'feature_level',
-        'meta_value'     => $level,
-        'orderby'        => 'title',
-        'order'          => 'ASC',
-      ]);
+        <?php
+        $rappers = new WP_Query([
+          'post_type'      => 'artist',
+          'posts_per_page' => -1,
+          'tax_query'      => [
+            'relation' => 'AND',
+            [
+              'taxonomy' => 'feature_level',
+              'field'    => 'slug',
+              'terms'    => $feature_slug,
+            ],
+            [
+              'taxonomy' => 'artist_type',
+              'field'    => 'slug',
+              'terms'    => ['rapper'],
+              'operator' => 'IN',
+            ],
+          ],
+          'orderby'        => 'title',
+          'order'          => 'ASC',
+        ]);
 
-      if ($rappers->have_posts()): ?>
-        <div class="feature-group">
-          <h3 class="feature-level"><?php echo $label; ?></h3>
+        if ($rappers->have_posts()): ?>
           <div class="author-grid">
             <?php while ($rappers->have_posts()): $rappers->the_post();
               $portrait = get_field('portrait_image');
@@ -83,18 +112,21 @@ get_header();
               <div class="book-item">
                 <a href="<?php the_permalink(); ?>">
                   <?php if ($img_url): ?>
-                    <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title(); ?>">
+                    <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title_attribute(); ?>">
                   <?php endif; ?>
                   <h3><?php the_title(); ?></h3>
                 </a>
               </div>
             <?php endwhile; ?>
           </div>
-        </div>
-        <?php wp_reset_postdata();
-      endif;
-    endforeach;
-    ?>
+        <?php
+        endif;
+        wp_reset_postdata();
+        ?>
+
+      </div> <!-- .feature-group rapper-group -->
+
+    <?php endforeach; ?>
   </section>
 
 </main>
