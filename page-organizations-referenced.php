c6a2c008128ff9e0fdfe9c6a47f15e3fe3ca6290
Stationary Reality <stationaryreality@tutanota.com>
2025-09-09 15:47:06 -0400

    removed acf field for featured orgs, will replace with taxonomy

--- page-organizations-referenced.php
+++ page-organizations-referenced.php
@@ -3,61 +3,18 @@
 get_header();
 ?>
 
-<h2 style="margin-bottom:1em;">Featured Organizations</h2>
+<h2 style="margin-bottom:1em;">Organizations</h2>
 <div class="song-grid">
 <?php
-$featured = new WP_Query([
+$orgs = new WP_Query([
   'post_type'      => 'organization',
   'posts_per_page' => -1,
-  'meta_key'       => 'is_featured',
-  'meta_value'     => 1,
   'orderby'        => 'title',
   'order'          => 'ASC',
 ]);
 
-if ($featured->have_posts()):
-  while ($featured->have_posts()): $featured->the_post();
-    $bio = get_field('org_bio');
-    $cover = get_field('cover_image');
-    $img_url = $cover ? $cover['sizes']['thumbnail'] : '';
-    ?>
-    <div class="book-item">
-      <a href="<?php the_permalink(); ?>">
-        <?php if ($img_url): ?>
-          <img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title(); ?>" style="aspect-ratio:1/1;object-fit:cover;">
-        <?php endif; ?>
-        <h3><?php the_title(); ?></h3>
-      </a>
-      <?php if ($bio): ?>
-        <p><?php echo esc_html(wp_trim_words($bio, 20)); ?></p>
-      <?php endif; ?>
-    </div>
-    <?php
-  endwhile;
-  wp_reset_postdata();
-endif;
-?>
-</div>
-
-<h2 style="margin-top:3em; margin-bottom:1em;">Referenced Organizations</h2>
-<div class="song-grid">
-<?php
-$referenced = new WP_Query([
-  'post_type'      => 'organization',
-  'posts_per_page' => -1,
-  'meta_query' => [
-    [
-      'key'     => 'is_featured',
-      'value'   => 1,
-      'compare' => '!=',
-    ]
-  ],
-  'orderby'        => 'title',
-  'order'          => 'ASC',
-]);
-
-if ($referenced->have_posts()):
-  while ($referenced->have_posts()): $referenced->the_post();
+if ($orgs->have_posts()):
+  while ($orgs->have_posts()): $orgs->the_post();
     $bio = get_field('org_bio');
     $cover = get_field('cover_image');
     $img_url = $cover ? $cover['sizes']['thumbnail'] : '';
