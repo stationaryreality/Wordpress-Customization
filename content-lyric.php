74aefce052566bc70607757d4de8698d94744cec
Stationary Reality <stationaryreality@tutanota.com>
2025-09-13 13:35:52 -0400

    added source to lyric page

--- content-lyric.php
+++ content-lyric.php
@@ -1,4 +1,5 @@
 <?php
+$source = get_field('song'); // Book, Reference, etc.
 ?>
 
 <div class="person-content">
@@ -7,6 +8,14 @@
   <div class="lyric-content">
     <?php the_content(); ?>
   </div>
+
+    <?php if ($source): ?>
+    <p class="lyric-source" style="margin-top:1em;">
+      Source: <a href="<?php echo esc_url(get_permalink($source->ID)); ?>">
+        <?php echo esc_html(get_the_title($source->ID)); ?>
+      </a>
+    </p>
+  <?php endif; ?>
 
   <?php get_template_part('content/lyric-nav'); ?>
 </div>
