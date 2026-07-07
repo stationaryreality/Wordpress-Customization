d61593e3a5745ac9109af461529667a6cc2e64bb
Stationary Reality <stationaryreality@tutanota.com>
2026-07-06 20:27:16 -0400

    debugging knowledge sections

--- inc/presentation/knowledge-sections.php
+++ inc/presentation/knowledge-sections.php
@@ -1,7 +1,46 @@
 <?php
-
+/**
+ * Render a collection of normalized knowledge sections.
+ *
+ * Each section should contain:
+ *  - type
+ *  - query
+ *  - info
+ *  - search_term
+ *  - term (optional)
+ */
 function kp_render_knowledge_sections(array $sections) {
 
-    return;
+    foreach ($sections as $section) {
 
+        $type  = $section['type'] ?? '';
+        $query = $section['query'] ?? null;
+
+        if (!$type) {
+            continue;
+        }
+
+        if (!$query instanceof WP_Query) {
+            continue;
+        }
+
+        if (!$query->have_posts()) {
+            continue;
+        }
+
+        $template = locate_template(
+            "template-parts/search/{$type}.php"
+        );
+
+        if (!$template) {
+
+            continue;
+        }
+
+        get_template_part(
+            "template-parts/search/{$type}",
+            null,
+            $section
+        );
+    }
 }
