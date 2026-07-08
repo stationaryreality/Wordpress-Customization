f088fc05c6c4549e456fe1e12b6b341ec6f580a9
Stationary Reality <stationaryreality@tutanota.com>
2026-07-08 15:14:18 -0400

    added backwards compatible layer in knowledge-resolver for concept.php to work during transition

--- inc/collectors/knowledge-resolver.php
+++ inc/collectors/knowledge-resolver.php
@@ -85,3 +85,67 @@ function kp_resolve_knowledge(string $name) {
     return null;
 
 }
+
+
+//temporary backwards compat - 2026-07-08
+
+function kp_find_portal($name)
+{
+    $result = kp_resolve_knowledge($name);
+
+    return ($result && $result['type'] === 'portal')
+        ? $result['object']
+        : null;
+}
+
+function kp_find_concept($name)
+{
+    $result = kp_resolve_knowledge($name);
+
+    return ($result && $result['type'] === 'concept')
+        ? $result['object']
+        : null;
+}
+
+function kp_find_topic($name)
+{
+    $result = kp_resolve_knowledge($name);
+
+    return ($result && $result['type'] === 'topic')
+        ? $result['term']
+        : null;
+}
+
+function kp_find_theme($name)
+{
+    $result = kp_resolve_knowledge($name);
+
+    return ($result && $result['type'] === 'theme')
+        ? $result['term']
+        : null;
+}
+
+function kp_resolve_primary_destination($name)
+{
+    $result = kp_resolve_knowledge($name);
+
+    if (!$result) {
+
+        return [
+            'type'   => null,
+            'object' => null,
+        ];
+
+    }
+
+    return [
+
+        'type'   => $result['type'],
+
+        'object' =>
+            $result['object']
+            ?? $result['term']
+            ?? null,
+
+    ];
+}
