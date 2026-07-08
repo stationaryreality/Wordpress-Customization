a316adbd00029e25019b0d51ad43f7ea98b4a1f2
Stationary Reality <stationaryreality@tutanota.com>
2026-07-08 14:46:32 -0400

    added knowledge view, replaced knowledge-builder.php

--- inc/presentation/knowledge-builder.php
+++ inc/presentation/knowledge-builder.php
@@ -1,95 +1,46 @@
 <?php
 
-/**
- * Build normalized knowledge sections.
- */
+function kp_build_knowledge(array $sections, array $args = [])
+{
+    $active_sections = [];
+    $total_entries   = 0;
 
-function kp_build_knowledge_sections(array $resolved) {
+    foreach ($sections as $type => $items) {
 
-    $sections = [];
-
-    /*
-    ---------------------------------------
-    Determine taxonomy
-    ---------------------------------------
-    */
-
-    switch ($resolved['type']) {
-
-        case 'topic':
-
-            $taxonomy = 'topic';
-            $term = $resolved['term'];
-            break;
-
-        case 'theme':
-
-            $taxonomy = 'theme';
-            $term = $resolved['term'];
-            break;
-
-        default:
-
-            return [];
-
-    }
-
-    /*
-    ---------------------------------------
-    CPTs
-    ---------------------------------------
-    */
-
-    foreach (get_cpt_metadata() as $type => $info) {
-
-        if (in_array($type, [
-            'featured_artists',
-            'other_artists',
-            'songs_referenced',
-        ])) {
+        if (empty($items)) {
             continue;
         }
 
-        $query = new WP_Query([
+        $active_sections[$type] = count($items);
 
-            'post_type' => $type,
-
-            'posts_per_page' => -1,
-
-            'orderby' => 'title',
-
-            'order' => 'ASC',
-
-            'tax_query' => [[
-
-                'taxonomy' => $taxonomy,
-                'field' => 'term_id',
-                'terms' => $term->term_id,
-
-            ]]
-
-        ]);
-
-        if (!$query->have_posts()) {
-            continue;
-        }
-
-        $sections[] = [
-
-            'type' => $type,
-
-            'query' => $query,
-
-            'info' => $info,
-
-            'search_term' => $term->name,
-
-            'term' => $term,
-
-        ];
-
+        $total_entries += count($items);
     }
 
-    return $sections;
+    return [
 
+        'hero' => $args['hero'] ?? null,
+
+        'sections' => $sections,
+
+        'active_sections' => $active_sections,
+
+        'total_entries' => $total_entries,
+
+        'section_order' =>
+            $args['section_order']
+            ?? array_keys($sections),
+
+        'section_labels' =>
+            $args['section_labels']
+            ?? [],
+
+        'map' =>
+            $args['map']
+            ?? get_cpt_metadata(),
+
+        'context' =>
+            $args['context']
+            ?? [],
+
+    ];
 }
