e2243c4e274063ff1ee86cb1508973de2c2df750
Stationary Reality <stationaryreality@tutanota.com>
2026-07-09 09:06:00 -0400

    knowledge updated to temporary fix

--- inc/presentation/views/knowledge.php
+++ inc/presentation/views/knowledge.php
@@ -1,19 +1,8 @@
 <?php
 
-$knowledge_data = kp_build_portal_sections(
-    $query,
-    $map,
-    $section_order,
-    $section_labels,
-    [
-        'type'    => 'portal',
-        'post_id' => get_the_ID(),
-    ]
-);
-
 kp_load_knowledge_view(
-    'knowledge',
+    'index',
     [
-        'portal_data' => $knowledge_data,
+        'portal_data' => $portal_data,
     ]
 );
