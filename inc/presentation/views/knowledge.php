8a32622bf941b1aa78d80e0d7989ce8cf32e294b
Stationary Reality <stationaryreality@tutanota.com>
2026-07-09 12:56:28 -0400

    updated knowledge view

--- inc/presentation/views/knowledge.php
+++ inc/presentation/views/knowledge.php
@@ -1,8 +1,29 @@
 <?php
 
-kp_load_knowledge_view(
-    'index',
-    [
-        'portal_data' => $portal_data,
-    ]
-);
+$sections = $portal_data['sections'] ?? [];
+
+$map = $portal_data['map'] ?? [];
+
+/*
+|--------------------------------------------------------------------------
+| RENDER SECTIONS
+|--------------------------------------------------------------------------
+*/
+
+foreach ($sections as $type => $items) {
+
+    if (empty($items)) {
+        continue;
+    }
+
+    get_template_part(
+        'template-parts/search/' . $type,
+        null,
+        [
+            'items' => $items,
+            'type'  => $type,
+            'info'  => $map[$type] ?? [],
+        ]
+    );
+
+}
