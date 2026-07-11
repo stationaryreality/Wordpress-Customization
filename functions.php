23df1c8351309e355cdd6b37def7b257a33be38f
Stationary Reality <stationaryreality@tutanota.com>
2026-07-11 07:55:13 -0400

    enabling functions

--- functions.php
+++ functions.php
@@ -479,11 +479,11 @@ require_once get_stylesheet_directory() . '/inc/references.php';
 
 //require_once get_stylesheet_directory() . '/inc/presentation/load-view.php';
 
-//require_once get_stylesheet_directory() . '/inc/presentation/cards/build-card.php';
+require_once get_stylesheet_directory() . '/inc/presentation/cards/build-card.php';
 
-//require_once get_stylesheet_directory() . '/inc/collectors/knowledge-collector.php';
+require_once get_stylesheet_directory() . '/inc/collectors/knowledge-collector.php';
 
-//require_once get_stylesheet_directory() . '/inc/presentation/knowledge-registry.php';
+require_once get_stylesheet_directory() . '/inc/presentation/knowledge-registry.php';
 
 // above is temp disable for updraft error
 
