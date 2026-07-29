cc0b20c72d6c019d6656a15f79e55490317dd086
Stationary Reality <stationaryreality@tutanota.com>
2026-07-29 11:33:55 -0400

    major css restructuring, one file at a time, footnote, excerpt

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -72,3 +72,6 @@ wp_enqueue_style('component-footnotes-cpt-videos', get_stylesheet_directory_uri(
 wp_enqueue_style('component-footnotes-song-videos', get_stylesheet_directory_uri() . '/assets/css/components/footnotes/song-videos.css', [], filemtime(get_stylesheet_directory() . '/assets/css/components/footnotes/song-videos.css'));
 
 wp_enqueue_style('component-footnotes-elements', get_stylesheet_directory_uri() . '/assets/css/components/footnotes/elements.css', [], filemtime(get_stylesheet_directory() . '/assets/css/components/footnotes/elements.css'));
+
+
+wp_enqueue_style('component-footnotes-excerpts', get_stylesheet_directory_uri() . '/assets/css/components/footnotes/excerpts.css', [], filemtime(get_stylesheet_directory() . '/assets/css/components/footnotes/excerpts.css'));
