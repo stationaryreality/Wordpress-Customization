ccd8014316ac2e257a9cfdb198976157fa46377d
Stationary Reality <stationaryreality@tutanota.com>
2026-07-30 12:50:48 -0400

    fixing image nav as model for other cpts

--- template-parts/navigation/image.php
+++ template-parts/navigation/image.php
@@ -15,32 +15,34 @@ $prev_id = $image_ids[$current_index - 1] ?? null;
 
 <div class="cpt-image-nav-top">
   <?php if ($prev_id): ?>
-    <a href="<?php echo get_permalink($prev_id); ?>" class="cpt-image-nav-prev cpt-keyboard-nav-prev">
-      <?php
-      $cover = get_field('image_file', $prev_id);
-      $thumb_url = ($cover && isset($cover['sizes']['thumbnail'])) ? $cover['sizes']['thumbnail'] : get_the_post_thumbnail_url($prev_id, 'thumbnail');
-      if ($thumb_url): ?>
+    <?php
+    $cover = get_field('image_file', $prev_id);
+    $thumb_url = ($cover && isset($cover['sizes']['thumbnail'])) ? $cover['sizes']['thumbnail'] : get_the_post_thumbnail_url($prev_id, 'thumbnail');
+    ?>
+    <a href="<?php echo get_permalink($prev_id); ?>" class="cpt-image-nav-prev">
+      <?php if ($thumb_url): ?>
         <img src="<?php echo esc_url($thumb_url); ?>" alt="<?php echo esc_attr(get_the_title($prev_id)); ?>" class="cpt-image-nav-thumb">
       <?php endif; ?>
-      <span class="cpt-image-nav-label">← Previous</span>
+      <span class="cpt-image-nav-label">← <?php echo esc_html(get_the_title($prev_id)); ?></span>
     </a>
   <?php endif; ?>
 
   <?php if ($prev_id || $next_id): ?>
-    <span class="cpt-keyboard-hint-inline" title="Use ← → arrow keys to navigate">
-      ⌨️
+    <span class="cpt-keyboard-hint-inline" title="Use arrow keys to navigate">
+      Use ← ⌨️ → keys
     </span>
   <?php endif; ?>
 
   <?php if ($next_id): ?>
-    <a href="<?php echo get_permalink($next_id); ?>" class="cpt-image-nav-next cpt-keyboard-nav-next">
-      <?php
-      $cover = get_field('image_file', $next_id);
-      $thumb_url = ($cover && isset($cover['sizes']['thumbnail'])) ? $cover['sizes']['thumbnail'] : get_the_post_thumbnail_url($next_id, 'thumbnail');
-      if ($thumb_url): ?>
+    <?php
+    $cover = get_field('image_file', $next_id);
+    $thumb_url = ($cover && isset($cover['sizes']['thumbnail'])) ? $cover['sizes']['thumbnail'] : get_the_post_thumbnail_url($next_id, 'thumbnail');
+    ?>
+    <a href="<?php echo get_permalink($next_id); ?>" class="cpt-image-nav-next">
+      <span class="cpt-image-nav-label"><?php echo esc_html(get_the_title($next_id)); ?> →</span>
+      <?php if ($thumb_url): ?>
         <img src="<?php echo esc_url($thumb_url); ?>" alt="<?php echo esc_attr(get_the_title($next_id)); ?>" class="cpt-image-nav-thumb">
       <?php endif; ?>
-      <span class="cpt-image-nav-label">Next →</span>
     </a>
   <?php endif; ?>
 </div>
