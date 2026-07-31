dc1ed89ab3543f82ff14f06e5e1e0e165377921a
Stationary Reality <stationaryreality@tutanota.com>
2026-07-31 07:59:43 -0400

    css project, debugging, disabling loader to test atlas view side scroll

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -55,33 +55,7 @@ foreach ($css_files as $file) {
 }
 
 
-// === NEW STYLE LOADER (Loads all refactored CSS) ===
-function enqueue_new_style_files() {
-    $base_path = get_stylesheet_directory() . '/assets/css';
-    $base_uri = get_stylesheet_directory_uri() . '/assets/css';
-    
-    // Define folder structure to load
-    $folders = [
-        'global' => glob("{$base_path}/global/*.css"), // ← Changed to glob()
-        'cpt' => glob("{$base_path}/cpt/*.css"),
-        'pages' => glob("{$base_path}/pages/*.css"),
-        'components' => glob("{$base_path}/components/*.css"),
-        'components/footnotes' => glob("{$base_path}/components/footnotes/*.css"),
-        'admin' => glob("{$base_path}/admin/*.css"),
-    ];
-    
-    foreach ($folders as $folder_name => $files) {
-        if (is_array($files)) {
-            foreach ($files as $file_path) {
-                $file_name = basename($file_path);
-                $handle = 'new-' . $folder_name . '-' . basename($file_name, '.css');
-                $relative_path = str_replace($base_path . '/', '', $file_path);
-                wp_enqueue_style($handle, "{$base_uri}/{$relative_path}", [], filemtime($file_path));
-            }
-        }
-    }
-}
-add_action('wp_enqueue_scripts', 'enqueue_new_style_files');
+
 
 // === KEYBOARD NAVIGATION SCRIPT ===
 function enqueue_keyboard_navigation() {
