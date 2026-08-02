8c546e424309b201f9fa3ed299a20fa163fa84ea
Stationary Reality <stationaryreality@tutanota.com>
2026-08-02 08:15:37 -0400

    migrated home page to new grid, css, deleted eleemnts and image-grid legacy css, again

--- template-parts/grids/home.php
+++ template-parts/grids/home.php
@@ -12,12 +12,12 @@ if (empty($pages)) {
 <section class="homepage-section">
     
     <?php if ($title) : ?>
-        <h2 class="homepage-section-title">
+        <h2 class="page-section-title">
             <?php echo esc_html($title); ?>
         </h2>
     <?php endif; ?>
 
-    <div class="homepage-grid">
+    <div class="tag-posts-grid">
 
         <?php foreach ($pages as $item) :
 
@@ -29,37 +29,35 @@ if (empty($pages)) {
 
             setup_postdata($page);
             
-            // Get the excerpt from the database, fallback to the PHP array description
             $excerpt = get_the_excerpt($page->ID);
             if ( empty($excerpt) ) {
                 $excerpt = $item['description'];
             }
         ?>
 
-            <article class="homepage-item">
+            <div class="tag-post-item">
 
-                <a href="<?php echo esc_url(get_permalink($page->ID)); ?>" class="homepage-item-thumbnail">
+                <a href="<?php echo get_permalink($page->ID); ?>" class="tag-post-thumbnail">
                     <?php if (has_post_thumbnail($page->ID)) : ?>
                         <?php echo get_the_post_thumbnail($page->ID, 'medium'); ?>
                     <?php endif; ?>
                 </a>
 
-                <a href="<?php echo esc_url(get_permalink($page->ID)); ?>" class="homepage-item-title">
+                <a href="<?php echo get_permalink($page->ID); ?>" class="tag-post-title">
                     <?php echo esc_html($item['title']); ?>
                 </a>
 
-                <p class="homepage-item-excerpt">
+                <p class="tag-post-excerpt">
                     <?php 
                     $raw_excerpt = get_the_excerpt($page->ID);
                     if ( empty($raw_excerpt) ) {
                         $raw_excerpt = $item['description'];
                     }
-                    // Strip any rogue HTML tags injected by other plugins
                     echo esc_html( wp_strip_all_tags($raw_excerpt) ); 
                     ?>
                 </p>
 
-            </article>
+            </div>
 
         <?php endforeach; ?>
 
--- templates/pages/home.php
+++ templates/pages/home.php
@@ -1,21 +1,20 @@
 <?php
 /* Template Name: Custom Home */
-
 get_header();
 
 $homepage_sections = site_get_navigation_sections();
 ?>
 
-<main class="site-main homepage-posts homepage-sectioned">
+<main class="homepage-posts homepage-sectioned">
 
-    <?php foreach ($homepage_sections as $section_title => $pages) : ?>
-        <?php
-        get_template_part('template-parts/grids/home', null, [
-            'title' => $section_title,
-            'pages' => $pages,
-        ]);
-        ?>
-    <?php endforeach; ?>
+<?php foreach ($homepage_sections as $section_title => $pages) : ?>
+    <?php
+    get_template_part('template-parts/grids/home', null, [
+        'title' => $section_title,
+        'pages' => $pages,
+    ]);
+    ?>
+<?php endforeach; ?>
 
 </main>
 
