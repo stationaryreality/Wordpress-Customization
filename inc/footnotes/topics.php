0fa35955ba27c035b1589dbeb699885ae1e5d27b
Stationary Reality <stationaryreality@tutanota.com>
2026-08-31 18:58:25 -0400

    disabling undoing taxonomy changes in topics

--- inc/footnotes/topics.php
+++ inc/footnotes/topics.php
@@ -5,11 +5,10 @@
 // ===============================
 
 function fn_topics($chapter_id, $group_titles) {
-    $taxonomy_context = kp_build_taxonomy_context($chapter_id);
-    $topics = $taxonomy_context['topic'];
-        if (empty($topics)) {
-            return '';
-        }
+    $topics = get_the_terms($chapter_id, 'topic');
+    if (!$topics || is_wp_error($topics)) {
+        return '';
+    }
 
     usort($topics, fn($a, $b) => strcmp($a->name, $b->name));
 
